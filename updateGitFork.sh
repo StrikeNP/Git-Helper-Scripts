84afd18e18f0a509a40016cd7128c6351509146b
Nicolas Strike <strike@uwm.edu>
2018-10-17 18:01:53 -0500

    Update updateGitFork.sh
    Add support for both mirror pushes and individual branches.

--- updateGitFork.sh
+++ updateGitFork.sh
@@ -9,12 +9,14 @@
 #    * source branch: branch you want to pull updates from (defaults to master)
 #    * destination branch: branch you want to push updates to (defaults to master)
 #    * force update: adds the '-f' flag to the push command, thus force pushes
+#    * mirror (-m): update all branches (as long as they have the same name)
 #
 # Author: Nicolas Strike, 2018
 ##################################################################
 workdir=temprepo
 srcbranch=master
 destbranch=master
+mirror=0
 forceupdate=0
     while [ "$1" != "" ]; do
         case $1 in
@@ -32,6 +34,8 @@ forceupdate=0
                                     ;;
             -f | --force-update )   forceupdate=1
                                     ;;
+            -m | --mirror )         mirror=1
+                                    ;;
             -h | --help )           usage
                                     exit
                                     ;;
@@ -41,17 +45,25 @@ done
 
 
 rm -rf $workdir
-git clone $srcrepo $workdir
+git clone $srcrepo $workdir --mirror
 cd $workdir
 git remote add destination_repo $destrepo
 
+#override destbranch if user wants to mirror so all branches are pushed
+if [ $mirror -eq 1 ]
+then
+       destbranch="--mirror"
+fi
+
 if [ $forceupdate -eq 1 ]
 then
-      echo "Force pushing updates from $srcrepo:$srcbranch to $destrepo:$destbranch"
-      git push -f -u destination_repo $destbranch
+       echo "Force pushing updates from $srcrepo:$srcbranch to $destrepo:$destbranch"
+       git push -f -u destination_repo $destbranch
+       errorsDetected=$?
 else
-      echo "Pushing updates from $srcrepo:$srcbranch to $destrepo:$destbranch"
-      git push -u destination_repo $destbranch
+       echo "Pushing updates from $srcrepo:$srcbranch to $destrepo:$destbranch"
+       git push -u destination_repo $destbranch
+       errorsDetected=$?
 fi
 
 wait
